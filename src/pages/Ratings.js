14f4695f4f46899990a4a39381a687cc447c222b
dvp213 <108648545+dvp213@users.noreply.github.com>
2024-01-04 19:57:13 +0530

    rating

--- src/pages/Ratings.js
+++ src/pages/Ratings.js
@@ -11,33 +11,33 @@ function Ratings() {
     return (
         <>
             <Navigationbar />
-                <div className="background-photo">
-                    <div className="flex">
-                        <Ratingcard />
-                        <Ratingcard />
-                        <Ratingcard />
-                        <Ratingcard />
-                        <Ratingcard />
-                    </div>
-
-                    <Rating.Advanced percentFilled={70} className="mb-2">
-                        5 star
-                    </Rating.Advanced>
-                    <Rating.Advanced percentFilled={2} className="mb-2">
-                        4 star
-                    </Rating.Advanced>
-                    <Rating.Advanced percentFilled={8} className="mb-2">
-                        3 star
-                    </Rating.Advanced>
-                    <Rating.Advanced percentFilled={4} className="mb-2">
-                        2 star
-                    </Rating.Advanced>
-                    <Rating.Advanced percentFilled={1}>1 star</Rating.Advanced>
-
-                    <p className="mb-4 text-sm font-medium text-gray-500 dark:text-gray-400">1,745 global ratings</p>
-                    <Footerbar />
+            <div className="background-photo">
+                <div className="flex">
+                    <Ratingcard />
+                    <Ratingcard />
+                    <Ratingcard />
+                    <Ratingcard />
+                    <Ratingcard />
                 </div>
-            
+
+                <Rating.Advanced percentFilled={70} className="mb-2">
+                    5 star
+                </Rating.Advanced>
+                <Rating.Advanced percentFilled={2} className="mb-2">
+                    4 star
+                </Rating.Advanced>
+                <Rating.Advanced percentFilled={8} className="mb-2">
+                    3 star
+                </Rating.Advanced>
+                <Rating.Advanced percentFilled={4} className="mb-2">
+                    2 star
+                </Rating.Advanced>
+                <Rating.Advanced percentFilled={1}>1 star</Rating.Advanced>
+
+                <p className="mb-4 text-sm font-medium text-gray-500 dark:text-gray-400">1,745 global ratings</p>
+                <Footerbar />
+            </div>
+
         </>
     );
 }
