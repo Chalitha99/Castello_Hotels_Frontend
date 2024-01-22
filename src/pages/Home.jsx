d51e422054df160f772f4dba8cd03c32af3bc28d
Chalitha Mallawaarachchi <chalithathilakshana@gmail.com>
2024-01-22 09:50:17 +0530

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { Footer } from "flowbite-react";
 import Navigationbar from "../components/Navigationbar";
 import Footerbar from "../components/Footerbar";
 import "../css/home.css";
+import Home_Section from "../components/Home_Section";
+import Service from "../components/Service";
 
 function Home() {
     return (<>
@@ -13,7 +15,8 @@ function Home() {
             <h1 className='text-center text-white welcome'>Welcome to the Castello</h1>
             <h3></h3>
         </div>
-
+        <Home_Section></Home_Section>
+        <Service></Service>
         <Footerbar></Footerbar>
     
     </>  );
