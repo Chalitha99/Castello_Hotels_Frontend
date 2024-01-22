5d4055b9b9a8c2019d0fc4f6b872ba680cd4bd7c
dvp213 <108648545+dvp213@users.noreply.github.com>
2024-01-22 09:51:28 +0530

    review

--- src/pages/Ratings.js
+++ src/pages/Ratings.js
@@ -6,35 +6,41 @@ import Navigationbar from '../components/Navigationbar';
 import Footerbar from '../components/Footerbar';
 import Ratingcard from '../components/Ratingcard';
 import "../css/ratingpage.css";
+import Paginationforrating from '../components/Paginationforrating';
 
 function Ratings() {
     return (
         <>
             <Navigationbar />
-            <div className="background-photo">
-                <div className="flex">
-                    <Ratingcard />
-                    <Ratingcard />
-                    <Ratingcard />
-                    <Ratingcard />
-                    <Ratingcard />
+            <div className="background-photo h-screen h-[100%]">
+                <div className='middle-content'>
+                    <div className="flex">
+                        <Ratingcard />
+                        <Ratingcard />
+                        <Ratingcard />
+                        <Ratingcard />
+                        <Ratingcard />
+                    </div>
+                    <Paginationforrating />
+                    <div className="starrating">
+                        <Rating.Advanced percentFilled={50} className="mb-2">
+                            5 star
+                        </Rating.Advanced>
+                        <Rating.Advanced percentFilled={37} className="mb-2">
+                            4 star
+                        </Rating.Advanced>
+                        <Rating.Advanced percentFilled={8} className="mb-2">
+                            3 star
+                        </Rating.Advanced>
+                        <Rating.Advanced percentFilled={4} className="mb-2">
+                            2 star
+                        </Rating.Advanced>
+                        <Rating.Advanced percentFilled={1}>1 star</Rating.Advanced>
+                        <p className="globle_rating">1,745 Global Ratings</p>
+                    </div>
+
                 </div>
 
-                <Rating.Advanced percentFilled={70} className="mb-2">
-                    5 star
-                </Rating.Advanced>
-                <Rating.Advanced percentFilled={2} className="mb-2">
-                    4 star
-                </Rating.Advanced>
-                <Rating.Advanced percentFilled={8} className="mb-2">
-                    3 star
-                </Rating.Advanced>
-                <Rating.Advanced percentFilled={4} className="mb-2">
-                    2 star
-                </Rating.Advanced>
-                <Rating.Advanced percentFilled={1}>1 star</Rating.Advanced>
-
-                <p className="mb-4 text-sm font-medium text-gray-500 dark:text-gray-400">1,745 global ratings</p>
                 <Footerbar />
             </div>
 
